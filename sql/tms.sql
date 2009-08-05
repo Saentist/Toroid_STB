e55ad7784294e3485acc7d43e3737b648509bed5
Jesse Norell <jesse@kci.net>
2009-08-05 11:56:45 -0600

    tms_schedules is done, and tested working on import

--- sql/tms.sql
+++ sql/tms.sql
@@ -31,30 +31,37 @@ CREATE TABLE `tms_schedules` (
   `tf_cable_in_the_classroom`	char(1) NOT NULL,
   `tf_tv_rating`	char(4) NOT NULL,
   `tf_sap`		char(1) NOT NULL,
-  `tf_user_data1`	char(4) NOT NULL,
-  `tf_sex_rating`	char(4) NOT NULL,
-  `tf_violence_rating`	char(4) NOT NULL,
-  `tf_language_rating`	char(4) NOT NULL,
-  `tf_dialog_rating`	char(4) NOT NULL,
-  `tf_fv_rating`	char(4) NOT NULL,
-  `tf_enhanced`		char(4) NOT NULL,
-  `tf_three_d`		char(4) NOT NULL,
-  `tf_letterbox`	char(4) NOT NULL,
-  `tf_hdtv`		char(4) NOT NULL,
-  `tf_dolby`		char(4) NOT NULL,
-  `tf_dvs`		char(4) NOT NULL,
-  `tf_user_data2`	char(4) NOT NULL,
-  `tf_new`		char(4) NOT NULL,
-  `tf_net_syn_source`	char(4) NOT NULL,
-  `tf_net_syn_type`	char(4) NOT NULL,
-  `tf_subject_to_blackout`	char(4) NOT NULL,
-  `tf_time_approximate`	char(4) NOT NULL,
-  `tf_dubbed`		char(4) NOT NULL,
-  `tf_dubbed_language`	char(4) NOT NULL,
-  `tf_ei`		char(4) NOT NULL,
-  `tf_sap_language`	char(4) NOT NULL,
-  `tf_subtitled_language`	char(4) NOT NULL,
-  `tf_left_in_progress`	char(4) NOT NULL
+  `tf_user_data1`	char(1) NOT NULL,
+  `tf_sex_rating`	char(1) NOT NULL,
+  `tf_violence_rating`	char(1) NOT NULL,
+  `tf_language_rating`	char(1) NOT NULL,
+  `tf_dialog_rating`	char(1) NOT NULL,
+  `tf_fv_rating`	char(1) NOT NULL,
+  `tf_enhanced`		char(1) NOT NULL,
+  `tf_three_d`		char(1) NOT NULL,
+  `tf_letterbox`	char(1) NOT NULL,
+  `tf_hdtv`		char(1) NOT NULL,
+  `tf_dolby`		char(5) NOT NULL,
+  `tf_dvs`		char(1) NOT NULL,
+  `tf_user_data2`	char(1) NOT NULL,
+  `tf_new`		char(3) NOT NULL,
+  `tf_net_syn_source`	char(10) NOT NULL,
+  `tf_net_syn_type`	char(21) NOT NULL,
+  `tf_subject_to_blackout`	char(20) NOT NULL,
+  `tf_time_approximate`	char(16) NOT NULL,
+  `tf_dubbed`		char(1) NOT NULL,
+  `tf_dubbed_language`	char(40) NOT NULL,
+  `tf_ei`		char(3) NOT NULL,
+  `tf_sap_language`	char(40) NOT NULL,
+  `tf_subtitled_language`	char(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Tribune schedules (skedrec.txt)';
 SET character_set_client = @saved_cs_client;
 
+create unique index station_date_time_idx on tms_schedules(tf_station_num, tf_air_date, tf_air_time);
+create index air_date_idx on tms_schedules(tf_air_date);
+
+/* these two will be foreign keys to the programs and stations tables */
+create index database_key_idx on tms_schedules(tf_database_key);
+create index station_num_idx on tms_schedules(tf_station_num);
+
+load data infile '/home/jesse/dev/toroid/sql/skedrec.txt' replace into table tms_schedules fields terminated by '|';
